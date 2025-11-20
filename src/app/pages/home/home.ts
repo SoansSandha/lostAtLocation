bc94c5c89ef1e48a6a89ec2d13bb4a2fe8c0f128
Soans <soanssandha@gmail.com>
2025-11-19 21:31:08 -0500

    adding room page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Navbar } from '../../components/navbar/navbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, Navbar, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink,  MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
